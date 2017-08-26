4e924ca77cb351a5b219fe3dc6bf33c6f4333263
Pierce Lopez <pierce.lopez@gmail.com>
2017-08-26 13:56:06 -0400

    make New() to take a nsq-like log func instead of Logger
    to properly integrate with nsqd log levels

--- diskqueue_test.go
+++ diskqueue_test.go
@@ -71,17 +71,10 @@ type tbLog interface {
 	Log(...interface{})
 }
 
-type testLogger struct {
-	tbLog
-}
-
-func (tl *testLogger) Output(maxdepth int, s string) error {
-	tl.Log(s)
-	return nil
-}
-
-func NewTestLogger(tbl tbLog) Logger {
-	return &testLogger{tbl}
+func NewTestLogger(tbl tbLog) AppLogFunc {
+	return func(lvl LogLevel, f string, args ...interface{}) {
+		tbl.Log(fmt.Sprintf(lvl.String()+": "+f, args...))
+	}
 }
 
 func TestDiskQueue(t *testing.T) {
